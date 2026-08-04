65bd3108f9ee8dfa1ae8f2be31977cc81bdb8828
Carlos Henrique <carlhenriquedev@gmail.com>
2026-08-04 20:52:57 -0300

    Separei responsabilidades

---
+++ backend/database.js
@@ -0,0 +1,36 @@
+// Dados de usuarios (Sabrina)
+// id, email, senha, nome
+
+
+
+
+
+
+// Dados de saldo (Carlos)
+// id, saldo_total, entradas, saidas, despesas, receita, id_usuario
+
+
+
+
+
+// Dados de Professor (Ewerton)
+// id, nome, cpf
+
+
+
+
+
+// Dados de Cursos (Matheus)
+// id, nome, cargahoraria, avaliação, modulo, genero, id_professor, id_usuario
+
+
+
+
+
+// Dados de To Do List (Marcio)
+// id_usuario, titulo, descricao
+
+
+
+
+
