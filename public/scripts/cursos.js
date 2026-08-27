bbe820e6cadd67e91747ef1021a622b7078db52d
matheus marques <matheus.mrkz@gmail.com>
2026-08-27 02:42:50 -0300

    feat e fix: adição da pasta de imagens, correção na lógica de cursos.js e ajuste nos dados dos cursos

--- public/scripts/cursos.js
+++ public/scripts/cursos.js
@@ -1,3 +1,5 @@
+const template = document.getElementById("template-curso");
+
 async function exibirCursos() {
     const response = await fetch("/api/cursos");
     const cursos = await response.json();
@@ -5,14 +7,19 @@ async function exibirCursos() {
     const container = document.getElementById("lista-de-cursos");
 
     Object.values(cursos).forEach(curso => {
-        const div = document.createElement("div");
-        div.innerHTML = `
-            <h2>${curso.nome}</h2>
-            <p>${curso.descricao}</p>
-            <p>Carga horária: ${curso.carga_horaria}h — Avaliação: ${curso.avaliacao}</p>
-        `;
-        container.appendChild(div);
-    });
+        const clone = template.content.cloneNode(true);
+
+        const imagem = clone.querySelector(".card-curso__imagem");
+        imagem.src = `/images/cursos/${curso.imagem}`;
+        imagem.widht = 200;
+        imagem.alt = curso.nome;
+        
+        clone.querySelector(".card-curso__titulo").textContent = curso.nome;
+        clone.querySelector(".card-curso__descricao").textContent = curso.descricao;
+        clone.querySelector(".card-curso__info").textContent = `Carga horária: ${curso.carga_horaria} — Preço: ${curso.preco}`;
+
+        container.appendChild(clone);
+})
 }
 
 exibirCursos();
